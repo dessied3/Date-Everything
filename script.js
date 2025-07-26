3d3230d416eed829d390b66b7e399c46c056d8ec
dessied3 <dessied3@icloud.com>
2025-07-26 16:44:47 -0400

    silly

--- script.js
+++ script.js
@@ -2,6 +2,6 @@ function myfunction() {
     let smarts ="";
     smarts = document.getElementById('smarts')
     if (smarts != 0){
-        print('successfully stored smarts points')
+        document.getElementById("result").innerHTML = "success"
     }
 };
